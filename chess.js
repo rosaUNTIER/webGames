1f399ece87befdcb33bc09d9eec8569499b72be8
oinkBob <23611873+oinkBob@users.noreply.github.com>
2018-03-02 19:09:24 +0100

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,2 +1,25 @@
 var board = document.getElementById('chess');
 var ctx = board.getContext('2d');
+var board = document.getElementById('board');
+var ctx = board.getContext('2d');
+
+//how big is one field in pixels
+const FIELD = 80;
+//chess .. uh .. 
+var colors = [
+white,
+black
+];
+//where those fields are
+var xPos = 0,
+    yPos = 0,
+    
+//circling through the field
+for(j = 0; j <= 7; j++){
+if (j%2 == 0){
+  	ctx.beginPath();
+  	ctx.rect(xPos*, k*pos, FIELD, FIELD);
+		var color = colors[0];
+		ctx.fillStyle = color;
+		ctx.fill();
+}
